21692c8255fecd1a8b06ff3dfe2e4db12c935581
YoshyFlores <yoshy.floresg17@gmail.com>
2023-05-16 18:25:29 -0600

    Practica 17

---
+++ api_laravel/app/Models/Post.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    //extracto del texto content
+    public function getExcerptAttribute(){
+        return substr($this->content, 0,150);
+    }
+
+    //mostrar la fecha de publicacion
+    public function getPublishedAttribute(){
+        return $this->create_at->format('d/m/Y');
+    }
+
+    //el usuario del post
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    
+    
+}
