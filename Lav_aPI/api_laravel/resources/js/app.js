d5e7446317053facc9dcad0c32e9fe8a6c25b42c
YoshyFlores <yoshy.floresg17@gmail.com>
2023-06-11 23:02:28 -0600

    Pract

---
+++ Lav_aPI/api_laravel/resources/js/app.js
@@ -0,0 +1,6 @@
+import './bootstrap';
+import {createApp} from 'vue';
+
+import PostListComponent from './components/PostListComponent.vue';
+
+createApp(PostListComponent).mount('#app')
---
+++ api_laravel/resources/js/components/PostListComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container">
+        <h1>Post</h1>
+        <br />
+        <ul v-for="post in posts" :key="post.id">
+            <li>{{ post.id }}
+            <p>{{post.post_name}}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default ({
+    data() {
+            return{
+                posts: []
+            }
+    },
+    created(){
+        this.getPosts();
+    },
+    methods: {
+        getPosts(api_url){
+            api_url = api_url || '/api/v2/posts'
+            const headers = {
+                'Authorization': 'Bearer 1|DQ9h7bvRM6zKXIgffBkBUcJIi1lgthjdom8Y1yh4'
+            };
+            fetch(api_url, {headers})
+                .then(response => response.json())
+                .then(response => this.posts = response.data)
+                .catch(error=>console.log(error))
+            }
+    }
+})
+</script>
+
+<style scoped></style>
